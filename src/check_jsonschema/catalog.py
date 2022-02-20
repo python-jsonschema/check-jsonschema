3d2d806833b0f4b25d7e40689ba61a55c6947774
Stephen Rosen <sirosen@globus.org>
2022-02-20 00:09:39 +0000

    Add Bamboo Spec to hooks
    Also some mild reformatting changes
    - correct capitalization of SchemaStore in hook descriptions
    - add a formatting helper for raw.githubusercontent URLs

--- src/check_jsonschema/catalog.py
+++ src/check_jsonschema/catalog.py
@@ -1,12 +1,22 @@
 import typing as t
 
+
+def _githubusercontent_url(owner: str, repo: str, ref: str, path: str) -> str:
+    return f"https://raw.githubusercontent.com/{owner}/{repo}/{ref}/{path}"
+
+
 # Known configs. The SchemaCatalog lists known schema URLs with their names.
 # kept in alphabetical order by name
 #
 # Additional config could be associated with the schemas in the future.
 SCHEMA_CATALOG: t.Dict[str, t.Dict[str, t.Any]] = {
     "azure-pipelines": {
-        "url": "https://raw.githubusercontent.com/microsoft/azure-pipelines-vscode/main/service-schema.json",  # noqa: E501
+        "url": _githubusercontent_url(
+            "microsoft", "azure-pipelines-vscode", "main", "service-schema.json"
+        ),
+    },
+    "bamboo-spec": {
+        "url": "https://json.schemastore.org/bamboo-spec.json",
     },
     "github-actions": {
         "url": "https://json.schemastore.org/github-action",
@@ -18,7 +28,12 @@ SCHEMA_CATALOG: t.Dict[str, t.Dict[str, t.Any]] = {
         "url": "https://www.schemastore.org/schemas/json/gitlab-ci.json",
     },
     "readthedocs": {
-        "url": "https://raw.githubusercontent.com/readthedocs/readthedocs.org/master/readthedocs/rtd_tests/fixtures/spec/v2/schema.json",  # noqa: E501
+        "url": _githubusercontent_url(
+            "readthedocs",
+            "readthedocs.org",
+            "master",
+            "readthedocs/rtd_tests/fixtures/spec/v2/schema.json",
+        ),
     },
     "renovate": {
         "url": "https://docs.renovatebot.com/renovate-schema.json",
