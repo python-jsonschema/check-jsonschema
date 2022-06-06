f7a56bfb1bc720906f87ee50eb04b6725e93a141
Stephen Rosen <sirosen@globus.org>
2022-06-06 03:09:27 +0000

    Update vendor-schemas to run pre-commit fixers
    After a schema is downloaded in the vendor-schemas job, it could be
    fixed by pre-commit.ci automatically. The result is an extra push and
    noise on the auto-update PR. It's better if the CI job automatically
    runs pre-commit on the results (or if the vendored schemas were
    ignored under pre-commit; nearly equivalent).
    This also ensures that if the only change is that the downloaded
    schema reverts the autofixing done under pre-commit, nothing will be
    recorded as changed.
    
    Update the gitlab schema by running `make vendor-schemas`.

--- scripts/vendor-schemas.py
+++ scripts/vendor-schemas.py
@@ -5,71 +5,139 @@ import datetime
 import hashlib
 import os
 import re
+import subprocess
 
 import requests
 
 from check_jsonschema.catalog import SCHEMA_CATALOG
 
-today = datetime.datetime.today().strftime("%Y-%m-%d")
+TODAY = datetime.datetime.today().strftime("%Y-%m-%d")
 
-vendor_slug = "\n<!-- vendor-insert-here -->\n"
+VENDOR_SLUG = "\n<!-- vendor-insert-here -->\n"
 
-_existing_changeline_pattern = re.compile(
-    re.escape(f"{vendor_slug}- Update vendored schemas")
+EXISTING_CHANGELINE_PATTERN = re.compile(
+    re.escape(f"{VENDOR_SLUG}- Update vendored schemas")
     + r" \(\d{4}-\d{2}-\d{2}\)"
     + "\n",
     flags=re.MULTILINE,
 )
 
+OLD_HASHES: dict[str, str] = {}
+UPDATED_SCHEMAS: set[str] = set()
+SCHEMAS_WITH_NEW_HASHES: set[str] = set()
 
-def download_schema(schema_name: str, schema_url: str) -> bool:
-    print(f"downloading {schema_name} schema to check ({schema_url})")
-    res = requests.get(schema_url)
+
+def schema2filename(name: str) -> str:
+    return f"src/check_jsonschema/builtin_schemas/vendor/{name}.json"
+
+
+def schema2hashfile(name: str) -> str:
+    return f"src/check_jsonschema/builtin_schemas/vendor/{name}.sha256"
+
+
+def file2digest(filepath: str) -> str:
     sha = hashlib.sha256()
-    sha.update(res.content)
-    new_digest = sha.hexdigest()
+    with open(filepath, "rb") as fp:
+        sha.update(fp.read())
+    return sha.hexdigest()
 
-    hashfile = f"src/check_jsonschema/builtin_schemas/vendor/{schema_name}.sha256"
-    if os.path.exists(hashfile):
-        with open(hashfile) as fp:
-            prev_digest: str | None = fp.read().strip()
-    else:
-        prev_digest = None
 
-    if new_digest == prev_digest:
-        return False
+def load_old_hashes() -> None:
+    for name, _config in SCHEMA_CATALOG.items():
+        hashfile = schema2hashfile(name)
+        if os.path.exists(hashfile):
+            with open(hashfile) as fp:
+                OLD_HASHES[name] = fp.read().strip()
 
-    with open(
-        f"src/check_jsonschema/builtin_schemas/vendor/{schema_name}.json", "wb"
-    ) as fp:
-        fp.write(res.content)
-    with open(
-        f"src/check_jsonschema/builtin_schemas/vendor/{schema_name}.sha256", "w"
-    ) as fp:
-        fp.write(new_digest)
 
-    return True
+def download_schemas() -> None:
+    print("downloading schemas to check for updates")
+    session = requests.Session()
+
+    for schema_name, config in SCHEMA_CATALOG.items():
+        schema_url = config["url"]
+
+        print(f"  {schema_name} ({schema_url})")
+        res = session.get(schema_url)
+        sha = hashlib.sha256()
+        sha.update(res.content)
+        new_digest = sha.hexdigest()
+
+        # early abort if downloaded content matches old hash
+        if new_digest == OLD_HASHES.get(schema_name):
+            continue
+
+        print("    content changed")
+        with open(schema2filename(schema_name), "wb") as fp:
+            fp.write(res.content)
+        UPDATED_SCHEMAS.add(schema_name)
+
+
+def run_precommit() -> None:
+    if not UPDATED_SCHEMAS:
+        print("no downloads updated, skipping pre-commit")
+        return
+
+    print("running pre-commit")
+    cmd = ["pre-commit", "run", "--files"]
+    for name in UPDATED_SCHEMAS:
+        cmd.append(schema2filename(name))
+    res = subprocess.run(cmd, capture_output=True)
+    if res.returncode == 0:
+        return
+
+
+def save_new_hashes() -> None:
+    print("saving new hashes")
+
+    def _save_new_hash(name: str, digest: str) -> None:
+        print(f"  - new hash for '{name}'")
+        hashfile = schema2hashfile(name)
+        with open(hashfile, "w", encoding="utf-8") as fp:
+            fp.write(digest)
+        SCHEMAS_WITH_NEW_HASHES.add(name)
+
+    for name in UPDATED_SCHEMAS:
+        digest = file2digest(schema2filename(name))
+
+        # new file, changes were made and hash should update
+        if name not in OLD_HASHES:
+            _save_new_hash(name, digest)
+
+        # if the existing hash does not match the new hash, save the new one
+        if digest != OLD_HASHES[name]:
+            _save_new_hash(name, digest)
+
+
+def check_changes_made() -> bool:
+    return bool(SCHEMAS_WITH_NEW_HASHES)
 
 
 def update_changelog() -> None:
+    print("changes were made, updating changelog")
     with open("CHANGELOG.md", encoding="utf-8") as fp:
         content = fp.read()
-    new_slug = vendor_slug + f"- Update vendored schemas ({today})\n"
+    new_slug = VENDOR_SLUG + f"- Update vendored schemas ({TODAY})\n"
 
-    if _existing_changeline_pattern.search(content):
-        content = re.sub(_existing_changeline_pattern, new_slug, content)
+    if EXISTING_CHANGELINE_PATTERN.search(content):
+        content = EXISTING_CHANGELINE_PATTERN.sub(new_slug, content)
     else:
-        content = content.replace(vendor_slug, new_slug)
+        content = content.replace(VENDOR_SLUG, new_slug)
     with open("CHANGELOG.md", "w", encoding="utf-8") as fp:
         fp.write(content)
 
 
 def main() -> None:
-    made_changes = False
-    for name, config in SCHEMA_CATALOG.items():
-        made_changes = download_schema(name, config["url"]) or made_changes
-    if made_changes:
+    print("vendor-schemas:BEGIN")
+    load_old_hashes()
+    download_schemas()
+    run_precommit()
+    save_new_hashes()
+    if check_changes_made():
         update_changelog()
+    else:
+        print("no changes detected")
+    print("vendor-schemas:END")
 
 
 if __name__ == "__main__":
