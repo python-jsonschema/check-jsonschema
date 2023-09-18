1908c945210e018bca0ca149b0fa7e81b40f072e
Stefan Weber <s.weber0688@gmail.com>
2023-09-18 17:04:14 +0200

    feat: add mkdocs-material schema to catalog

--- src/check_jsonschema/catalog.py
+++ src/check_jsonschema/catalog.py
@@ -110,6 +110,14 @@ SCHEMA_CATALOG: dict[str, dict[str, t.Any]] = {
             "types": "yaml",
         },
     },
+    "mkdocs-material": {
+        "url": "https://squidfunk.github.io/mkdocs-material/schema.json",
+        "hook_config": {
+            "name": "Validate MkDocs Material Config",
+            "files": r"^mkdocs\.yml$",
+            "types": "yaml",
+        },
+    },
     "readthedocs": {
         "url": _githubusercontent_url(
             "readthedocs",
