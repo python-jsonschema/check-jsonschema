20ab97ffcf23b6cc269cc3d5f8d2a3c61d889ba2
Stephen Rosen <sirosen@globus.org>
2023-12-05 17:25:59 -0600

    Add a test which verifies lazy file reading
    Create a directory of 2000 instance files, then iterate through them
    and verify that the number of open files never exceeds 2000 at any
    point during iteration.

---
+++ tests/unit/test_lazy_file_handling.py
@@ -0,0 +1,43 @@
+import os
+
+import pytest
+from click.testing import CliRunner
+
+from check_jsonschema.cli.main_command import build_checker
+from check_jsonschema.cli.main_command import main as cli_main
+
+
+@pytest.fixture
+def runner() -> CliRunner:
+    return CliRunner(mix_stderr=False)
+
+
+@pytest.mark.skipif(os.name != "posix", reason="test requires use of /proc/self/")
+def test_open_file_usage_never_exceeds_1000(runner, monkeypatch, tmp_path):
+    schema_path = tmp_path / "schema.json"
+    schema_path.write_text("{}")
+
+    args = [
+        "--schemafile",
+        str(schema_path),
+    ]
+
+    for i in range(2000):
+        instance_path = tmp_path / f"file{i}.json"
+        instance_path.write_text("{}")
+        args.append(str(instance_path))
+
+    checker = None
+
+    def fake_execute(argv):
+        nonlocal checker
+        checker = build_checker(argv)
+
+    monkeypatch.setattr("check_jsonschema.cli.main_command.execute", fake_execute)
+    res = runner.invoke(cli_main, args)
+    assert res.exit_code == 0, res.stderr
+
+    assert checker is not None
+    assert len(os.listdir("/proc/self/fd")) < 2000
+    for _fname, _data in checker._instance_loader.iter_files():
+        assert len(os.listdir("/proc/self/fd")), 2000
