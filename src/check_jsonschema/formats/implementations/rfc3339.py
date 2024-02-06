6a41b67f68c0885e56eb888d60e89e0bb8022e7b
Stephen Rosen <sirosen@globus.org>
2024-02-05 21:46:36 -0600

    Switch date-time validation to regex
    After benchmarking, it turns out that the manual, non-regex
    construction is significantly slower than using a regex to do most of
    the work. With a citation to the prior art in rfc3339-validator, this
    now implements a regex based check.

--- src/check_jsonschema/formats/implementations/rfc3339.py
+++ src/check_jsonschema/formats/implementations/rfc3339.py
@@ -1,118 +1,98 @@
-def validate(date_str: str) -> bool:
-    """Validate a string as a RFC3339 date-time.
+import re
+import typing as t
 
-    This check does the fastest possible validation of the date string (in Python),
-    deferring operations as much as possible to avoid unnecessary work.
-    """
-    try:
-        # the following chars MUST be fixed values:
-        #    YYYY-MM-DDTHH:MM:SSZ
-        #        ^  ^  ^  ^  ^
-        #
-        # so start by checking them first
-        # this keeps us as fast as possible in failure cases
-        #
-        # (note: "T" and "t" are both allowed under ISO8601)
-        if (
-            date_str[4] != "-"
-            or date_str[7] != "-"
-            or date_str[10] not in ("T", "t")
-            or date_str[13] != ":"
-            or date_str[16] != ":"
-        ):
-            return False
+# this regex is based on the one from the rfc3339-validator package
+# credit to the original author
+# original license:
+#
+#    MIT License
+#
+#    Copyright (c) 2019, Nicolas Aimetti
+#
+#    Permission is hereby granted, free of charge, to any person obtaining a copy
+#    of this software and associated documentation files (the "Software"), to deal
+#    in the Software without restriction, including without limitation the rights
+#    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#    copies of the Software, and to permit persons to whom the Software is
+#    furnished to do so, subject to the following conditions:
+#
+#    The above copyright notice and this permission notice shall be included in all
+#    copies or substantial portions of the Software.
+#
+#    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+#    SOFTWARE.
+#
+# modifications have been made for additional corner cases and speed
+RFC3339_REGEX = re.compile(
+    r"""
+    ^
+    (?:\d{4})
+    -
+    (?:0[1-9]|1[0-2])
+    -
+    (?:\d{2})
+    (?:T|t)
+    (?:[01]\d|2[0123])
+    :
+    (?:[0-5]\d)
+    :
+    (?:[0-5]\d)
+    # (optional) fractional seconds
+    (?:(\.|,)\d+)?
+    # UTC or offset
+    (?:
+        Z
+        | z
+        | [+-](?:[01]\d|2[0123]):[0-5]\d
+    )
+    $
+""",
+    re.VERBOSE | re.ASCII,
+)
 
-        # check for fractional seconds, which pushes the location of the offset/Z
-        # record the discovered start postiion of the offset segment
-        offset_start = 19
-        if date_str[19] in (".", ","):
-            offset_start = date_str.find("Z", 20)
-            if offset_start == -1:
-                offset_start = date_str.find("z", 20)
-            if offset_start == -1:
-                offset_start = date_str.find("+", 20)
-            if offset_start == -1:
-                offset_start = date_str.find("-", 20)
-            # if we can't find an offset after `.` or `,` as a separator, it's wrong
-            if offset_start == -1:
-                return False
 
-            # fractional seconds are checked to be numeric
-            # the spec seems to allow for any number of digits (?) so there's no
-            # length check here
-            frac_seconds = date_str[20:offset_start]
-            if not frac_seconds:
-                return False
-            if not frac_seconds.isnumeric():
-                return False
+def validate(date_str: t.Any) -> bool:
+    """Validate a string as a RFC3339 date-time."""
+    if not isinstance(date_str, str):
+        return False
+    if not RFC3339_REGEX.match(date_str):
+        return False
 
-        # now, handle Z vs offset
-        # (note: "Z" and "z" are both allowed under ISO8601)
-        z_offset = date_str[offset_start:] in ("Z", "z")
-        if z_offset and len(date_str) != offset_start + 1:
-            return False
-        if not z_offset:
-            if len(date_str) != offset_start + 6:
-                return False
-            if date_str[offset_start] not in ("+", "-"):
-                return False
-            if date_str[offset_start + 3] != ":":
-                return False
+    year, month, day = int(date_str[:4]), int(date_str[5:7]), int(date_str[8:10])
 
-        year = date_str[:4]
-        if not year.isnumeric():
-            return False
-        year_val = int(year)
-
-        month = date_str[5:7]
-        if not month.isnumeric():
-            return False
-        month_val = int(month)
-        if not 1 <= month_val <= 12:
-            return False
-
-        day = date_str[8:10]
-        if not day.isnumeric():
-            return False
+    if month in {4, 6, 9, 11}:
+        max_day = 30
+    elif month == 2:
+        max_day = 29 if year % 4 == 0 and (year % 100 != 0 or year % 400 == 0) else 28
+    else:
         max_day = 31
-        if month_val in (4, 6, 9, 11):
-            max_day = 30
-        elif month_val == 2:
-            max_day = (
-                29
-                if year_val % 4 == 0 and (year_val % 100 != 0 or year_val % 400 == 0)
-                else 28
-            )
-        if not 1 <= int(day) <= max_day:
-            return False
-
-        hour = date_str[11:13]
-        if not hour.isnumeric():
-            return False
-        if not 0 <= int(hour) <= 23:
-            return False
-        minute = date_str[14:16]
-        if not minute.isnumeric():
-            return False
-        if not 0 <= int(minute) <= 59:
-            return False
-        second = date_str[17:19]
-        if not second.isnumeric():
-            return False
-        if not 0 <= int(second) <= 59:
-            return False
-
-        if not z_offset:
-            offset_hour = date_str[offset_start + 1 : offset_start + 3]
-            if not offset_hour.isnumeric():
-                return False
-            if not 0 <= int(offset_hour) <= 23:
-                return False
-            offset_minute = date_str[offset_start + 4 : offset_start + 6]
-            if not offset_minute.isnumeric():
-                return False
-            if not 0 <= int(offset_minute) <= 59:
-                return False
-    except (IndexError, ValueError):
+    if not 1 <= day <= max_day:
         return False
     return True
+
+
+if __name__ == "__main__":
+    import timeit
+
+    N = 100_000
+    long_fracsec = "2018-12-31T23:59:59.8446519776713Z"
+    basic = "2018-12-31T23:59:59Z"
+    in_february = "2018-02-12T23:59:59Z"
+    in_february_invalid = "2018-02-29T23:59:59Z"
+
+    print("benchmarking")
+    for name, val in (
+        ("long_fracsec", long_fracsec),
+        ("basic", basic),
+        ("february", in_february),
+        ("february_invalid", in_february_invalid),
+    ):
+        all_times = timeit.repeat(
+            f"validate({val!r})", globals=globals(), repeat=3, number=N
+        )
+        print(f"{name} (valid={validate(val)}): {int(min(all_times) / N * 10**9)}ns")
