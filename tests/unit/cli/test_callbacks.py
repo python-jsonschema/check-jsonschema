08d04a7ea3f1cc3a3a5015f6effca091234caa5e
Stephen Rosen <sirosen@globus.org>
2024-07-05 10:21:36 -0500

    Refactor CLI unit tests + add deprecation tests
    - Move CLI unit tests to `tests/unit/cli/`
    - Add initial test cases for the deprecation callback helper

---
+++ tests/unit/cli/test_callbacks.py
@@ -0,0 +1,51 @@
+import click
+import pytest
+
+from check_jsonschema.cli.warnings import deprecation_warning_callback
+
+
+@click.command("foo")
+@click.option(
+    "--bar",
+    is_flag=True,
+    callback=deprecation_warning_callback("--bar", is_flag=True),
+)
+@click.option(
+    "--baz",
+    callback=deprecation_warning_callback(
+        "--baz", append_message="Use --frob instead!"
+    ),
+)
+def mycli(bar, baz):
+    print(bar)
+    if baz:
+        print(baz)
+
+
+def test_deprecation_warning_callback_on_missing_opts(runner):
+    result = runner.invoke(mycli, [])
+    assert result.exit_code == 0
+    assert result.stdout == "False\n"
+
+
+def test_deprecation_warning_callback_on_flag(runner):
+    with pytest.warns(
+        UserWarning,
+        match="'--bar' is deprecated and will be removed in a future release",
+    ):
+        result = runner.invoke(mycli, ["--bar"], catch_exceptions=False)
+    assert result.exit_code == 0, result.stdout
+    assert result.stdout == "True\n"
+
+
+def test_deprecation_warning_callback_added_message(runner):
+    with pytest.warns(
+        UserWarning,
+        match=(
+            "'--baz' is deprecated and will be removed in a future release. "
+            "Use --frob instead!"
+        ),
+    ):
+        result = runner.invoke(mycli, ["--baz", "ok"], catch_exceptions=False)
+    assert result.exit_code == 0, result.stdout
+    assert result.stdout == "False\nok\n"
